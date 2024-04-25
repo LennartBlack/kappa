e33b26b0a801363c3d7adfed667271ce262d9f4e
Lennart Schwartz <lennart.schwartz@ewe-netz.de>
2024-04-25 11:37:24 +0200

    Scripts für die DB

--- src/main/java/kappa/loadScripts/FileTest.java
+++ src/main/java/kappa/loadScripts/FileTest.java
@@ -12,20 +12,14 @@ public class FileTest {
 
         List<String> sqlImports = new ArrayList<>();
         List<String> sqlcreatetables = new ArrayList<>();
-        List<String> dropModZwei = new ArrayList<>();
+        List<String> dropAllNotMaxValuesAtAFixDate = new ArrayList<>();
 
-        // Pfad zum Verzeichnis
-        String verzeichnisPfad = "C:\\Users\\Lennart Schwartz\\OneDrive - IBS IT & Business School Oldenburg e. V\\Desktop\\messwerte";
+        //String verzeichnisPfad = "C:\\Users\\Lennart Schwartz\\OneDrive - IBS IT & Business School Oldenburg e. V\\Desktop\\messwerte";
+        String verzeichnisPfad = "C:\\ProgramData\\MySQL\\MySQL Server 8.3\\Uploads\\messwerte";
 
-        // Erstelle eine Dateiobjekt für das Verzeichnis
         File verzeichnis = new File(verzeichnisPfad);
 
-        // Überprüfe, ob das Verzeichnis existiert und ein Verzeichnis ist
         if (verzeichnis.exists() && verzeichnis.isDirectory()) {
-            // Erstelle eine ArrayList für die Dateinamen und zugehörigen Techplätze und Anlagen
-            List<String[]> dateiInformationen = new ArrayList<>();
-
-            // Erhalte eine Liste von Dateien im Verzeichnis
             File[] dateien = verzeichnis.listFiles();
 
             // Überprüfe, ob Dateien vorhanden sind
@@ -38,18 +32,32 @@ public class FileTest {
                         String techplatz = dateiName.substring(0, 4); // Annahme: Techplatz besteht aus den ersten 4 Zeichen
                         String anlage = dateiName.substring(9, 18); // Annahme: Anlage besteht aus den ersten 9 Zeichen
 
-                        // Speichere Techplatz und Anlage in einem Array
-                        String[] dateiInfo = {techplatz, anlage};
-
-
-                        // Löschen mod 2 = 1
-                        dropModZwei.add("delete from " + techplatz + " where mod(id, 2) = 1;");
-                        // Erstelle SQL-Import-Befehl
+                        // Fitler alle Maximalwerte für jeden Zeitstempel
+                        if(!dropAllNotMaxValuesAtAFixDate.contains("DELETE FROM " + techplatz +
+                        " WHERE (date, ampere, id) NOT IN ( " +
+                            " SELECT date, ampere, id " +
+                            " FROM (" +
+                                " SELECT date, ampere, id," +
+                                       " ROW_NUMBER() OVER (PARTITION BY date ORDER BY ampere DESC, id) AS row_num" +
+                                " FROM " + techplatz +
+                            ") AS temp" +
+                            " WHERE row_num = 1" +
+                        " );"))
+                        dropAllNotMaxValuesAtAFixDate.add("DELETE FROM " + techplatz +
+                        " WHERE (date, ampere, id) NOT IN ( " +
+                            " SELECT date, ampere, id " +
+                            " FROM (" +
+                                " SELECT date, ampere, id," +
+                                       " ROW_NUMBER() OVER (PARTITION BY date ORDER BY ampere DESC, id) AS row_num" +
+                                " FROM " + techplatz +
+                            ") AS temp" +
+                            " WHERE row_num = 1" +
+                        " );");
+                        
+                        // SQL-Befehl zum erstellen der Tabellen für jedes Kabel
                         if(!sqlcreatetables.contains("create table " + techplatz + " like ewes2;")){
                             sqlcreatetables.add("create table " + techplatz + " like ewes2;");
                         }
-                        // Füge das Array zur Liste hinzu
-                        dateiInformationen.add(dateiInfo);
                         sqlImports.add(
                         "LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.3/Uploads/messwerte/" + techplatz + "_____" + anlage + ".csv'" +
                         "INTO TABLE " + techplatz + 
@@ -61,29 +69,19 @@ public class FileTest {
                     }
                 }
             }
-
-            // Ausgabe der Dateiinformationen
-            System.out.println("Techplatz und Anlage für jede Datei:");
-            for (String[] info : dateiInformationen) {
-                System.out.println("Techplatz: " + info[0] + ", Anlage: " + info[1]);
-            }
-            System.out.println("SQL-Imports:");
-            for (int i = 0 ; i < sqlImports.size(); i++){
-                System.out.println(sqlImports.get(i));
-            }
         } else {
             System.out.println("Das Verzeichnis existiert nicht oder ist kein Verzeichnis.");
         }
-        //schreibeSqlImportsInDatei(sqlImports, "sql_imports.txt");
-        //schreibeSqlImportsInDatei(sqlcreatetables, "sqlcreatetables.txt");
-        //schreibeSqlImportsInDatei(dropModZwei, "dropModZwei.txt");
+        schreibeSqlImportsInDatei(sqlImports, "sql_imports.txt");
+        schreibeSqlImportsInDatei(sqlcreatetables, "sqlcreatetables.txt");
+        schreibeSqlImportsInDatei(dropAllNotMaxValuesAtAFixDate, "dropAllNotMaxValuesAtAFixDate.txt");
     }
     @SuppressWarnings("unused")
     private static void schreibeSqlImportsInDatei(List<String> sqlImports, String dateiName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(dateiName))) {
             for (String sqlImport : sqlImports) {
                 writer.write(sqlImport);
-                writer.newLine(); // Neue Zeile für jeden SQL-Import
+                writer.newLine();
             }
             System.out.println("SQL-Imports wurden in die Datei '" + dateiName + "' geschrieben.");
         } catch (IOException e) {
